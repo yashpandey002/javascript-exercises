f8f38a4ad176d0521931c47a5ffac58711f29a26
Yash Pandey <pandeyyash002@gmail.com>
2023-06-28 14:30:49 +0530

    Accidently commited the solution file previously

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let newString = "";
+    for (let i = 1; i <= num; i++) {
+        newString += string;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
