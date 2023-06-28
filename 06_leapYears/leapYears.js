3e20f5c06e207b007961cac1027d81395302966e
Yash Pandey <pandeyyash002@gmail.com>
2023-06-29 02:05:58 +0530

    Solved 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    if (typeof year !== "number") {
+        return "ERROR";
+    }
 
+    if (year <= 0) {
+        return "ERROR";
+    }
+
+    if (year % 4 === 0) {
+        if (year % 100 !== 0 || year % 400 === 0) {
+            return true;
+        }
+    }
+
+    return false;
 };
 
 // Do not edit below this line
